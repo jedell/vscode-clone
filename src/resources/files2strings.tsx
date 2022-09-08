e88cb82dbce7d418ff7b90bd8f626ff254a2010b
joe <jre2016@gmail.com>
2022-09-08 19:23:52 -0400

    adjust format of oberlin string

--- src/resources/files2strings.tsx
+++ src/resources/files2strings.tsx
@@ -49,18 +49,18 @@ const GS =
 const OBERLIN =
     `async function classes(url) {
 
-await fetch(url).then(function (response) {
-    return response.json();
-}).then(function (data) {
-    console.log(data);
-}).catch(function () {
-    console.log("Error");
-});
+  await fetch(url).then(function (response) {
+      return response.json();
+  }).then(function (data) {
+      console.log(data);
+  }).catch(function () {
+      console.log("Error");
+  });
 
 console.log('Systems Programming, Computer Science Theory, Algorithms, Privacy and Security')
 
 // Also Men's DIII Lacrosse
-    }
+}
     
     
 `
